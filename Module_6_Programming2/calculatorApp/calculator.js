fe05e56c69528815689b34e15d8a2992334b099c
Raul Veras <verasraul@gmail.com>
2021-08-14 02:55:31 -0400

    removed tests files

--- Module_6_Programming2/calculatorApp/calculator.js
+++ Module_6_Programming2/calculatorApp/calculator.js
@@ -14,10 +14,14 @@ const calculator = {
     } else {
       calculator.displayValue = displayValue === '0' ? digit : displayValue + digit;
     }
-    console.log(calculator);
   }
   
   function inputDecimal(dot) {
+    if (calculator.waitingForSecondOperand === true){
+        calculator.displayValue = '0.'
+        calculator.waitingForSecondOperand = false;
+        return
+    }
     if (!calculator.displayValue.includes(dot)) {
       calculator.displayValue += dot;
     }
@@ -26,14 +30,45 @@ const calculator = {
   function handleOperator(nextOperator) {
     const { firstOperand, displayValue, operator } = calculator
     const inputValue = parseFloat(displayValue);
+    
+    if (operator && calculator.waitingForSecondOperand)  {
+      calculator.operator = nextOperator;
+      return;
+    }
   
-    if (firstOperand === null && !isNaN(inputValue)) {
+  
+    if (firstOperand == null && !isNaN(inputValue)) {
       calculator.firstOperand = inputValue;
+    } else if (operator) {
+      const result = calculate(firstOperand, inputValue, operator);
+  
+      calculator.displayValue = `${parseFloat(result.toFixed(7))}`;
+      calculator.firstOperand = result;
     }
   
     calculator.waitingForSecondOperand = true;
     calculator.operator = nextOperator;
-    console.log(calculator);
+  }
+  
+  function calculate(firstOperand, secondOperand, operator) {
+    if (operator === '+') {
+      return firstOperand + secondOperand;
+    } else if (operator === '-') {
+      return firstOperand - secondOperand;
+    } else if (operator === '*') {
+      return firstOperand * secondOperand;
+    } else if (operator === '/') {
+      return firstOperand / secondOperand;
+    }
+  
+    return secondOperand;
+  }
+  
+  function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
   }
   
   function updateDisplay() {
@@ -46,27 +81,30 @@ const calculator = {
   const keys = document.querySelector('.calculator-keys');
   keys.addEventListener('click', (event) => {
     const { target } = event;
+    const { value } = target;
     if (!target.matches('button')) {
-        return;
-    }
-  
-    if (target.classList.contains('operator')) {
-      handleOperator(target.value);
-          updateDisplay();
       return;
     }
-  
-    if (target.classList.contains('decimal')) {
-      inputDecimal(target.value);
-          updateDisplay();
-      return;
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            inputDecimal(value);
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            if (Number.isInteger(parseFloat(value))) {
+                inputDigit(value);
+            }
     }
-  
-    if (target.classList.contains('all-clear')) {
-      console.log('clear', target.value);
-      return;
-    }
-  
-    inputDigit(target.value);
+
     updateDisplay();
   });
