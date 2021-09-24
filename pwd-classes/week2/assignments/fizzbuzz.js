52356fd6a0d809c50d0f6ba826e23d3b25d95b1f
Raul Veras <verasraul@gmail.com>
2021-09-24 02:08:54 -0400

    fizzbuzz

--- pwd-classes/week2/assignments/fizzbuzz.js
+++ pwd-classes/week2/assignments/fizzbuzz.js
@@ -1,7 +1,18 @@
-let x = 15
+// let x = 0
 let result = " ";
 
-if (x % 3 == 0 && x % 5 == 0){
+for (let x = 1; x < 61; x++){
+if (x % 3 === 0 && x % 5 === 0){
   result = "fizzbuzz";
   console.log(result);
-};
+  
+} else if (x % 3 == 0){
+  result = "fizz";
+  console.log(result);
+} else if (x % 5 == 0){
+  result = "buzz";
+  console.log(result);
+} else {
+  result = "x";
+  console.log(result);
+}};
